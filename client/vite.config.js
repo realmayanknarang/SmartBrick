1053f64c969b59e777428aa968d46cdd83d51e65
ANMOL GOYAL <anmolgoyal1974@gmail.com>
2026-07-03 16:44:02 +0530

    changed ui

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   test: {
     // Use jsdom to simulate a browser environment for React component tests
     environment: 'jsdom',
